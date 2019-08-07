f1be29f505647bf5eee2bed4ab22dec1e76f0985
Zard1096 <mr.zardqi@gmail.com>
2019-08-07 17:48:03 +0800

    Change SDK version

--- VideoPlsUtilsPlatformSDK/VideoPlsUtilsPlatformSDK/VideoPlsUtilsPlatformSDK/Common/Utils/VPUPGeneralInfo.m
+++ VideoPlsUtilsPlatformSDK/VideoPlsUtilsPlatformSDK/VideoPlsUtilsPlatformSDK/Common/Utils/VPUPGeneralInfo.m
@@ -14,7 +14,7 @@
 #import "VPUPNotificationCenter.h"
 #import <sys/utsname.h>
 
-NSString * const VideoPlsUtilsPlatformSDKVersion = @"1.3.0";
+NSString * const VideoPlsUtilsPlatformSDKVersion = @"1.3.1";
 NSString * const VPUPGeneralInfoSDKChangedNotification = @"VPUPGeneralInfoSDKChangedNotification";
 
 static NSString *IDFA = nil;
